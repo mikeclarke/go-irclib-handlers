a0cf7cc7dc145286bc3e93bd619e933fe79aca3c
Mike Clarke <mike@disqus.com>
2013-05-18 16:37:17 -0700

    Return early if not enough matches

--- youtube.go
+++ youtube.go
@@ -15,42 +15,44 @@ var message_RE = regexp.MustCompile(`(?:youtube|yt)(?: me)? (.*)`)
 func YouTube(event *irc.Event) {
 	matches := message_RE.FindStringSubmatch(event.Raw)
 
-	if len(matches) > 1 {
-		client := event.Client
-		buffer := event.Arguments[0]
+	if len(matches) < 2 {
+		return
+	}
 
-		query := matches[1]
-		baseUrl := "http://gdata.youtube.com/feeds/api/videos"
+	client := event.Client
+	buffer := event.Arguments[0]
 
-		params := url.Values{}
-		params.Add("orderBy", "relevance")
-		params.Add("max-results", "15")
-		params.Add("alt", "json")
-		params.Add("q", query)
+	query := matches[1]
+	baseUrl := "http://gdata.youtube.com/feeds/api/videos"
 
-		resp, err := http.Get(fmt.Sprintf("%s?%s", baseUrl, params.Encode()))
+	params := url.Values{}
+	params.Add("orderBy", "relevance")
+	params.Add("max-results", "15")
+	params.Add("alt", "json")
+	params.Add("q", query)
 
-		if err != nil {
-			client.Privmsg(buffer, fmt.Sprintf("error hitting youtube: %s", err))
-			return
-		}
+	resp, err := http.Get(fmt.Sprintf("%s?%s", baseUrl, params.Encode()))
 
-		// Decode response
-		var data map[string]interface{}
-		decoder := json.NewDecoder(resp.Body)
-		decoder.Decode(&data)
+	if err != nil {
+		client.Privmsg(buffer, fmt.Sprintf("error hitting youtube: %s", err))
+		return
+	}
 
-		// Pick a random video
-		feed := data["feed"].(map[string]interface{})
-		videos := feed["entry"].([]interface{})
-		video := videos[rand.Intn(len(videos))].(map[string]interface{})
-		links := video["link"].([]interface{})
+	// Decode response
+	var data map[string]interface{}
+	decoder := json.NewDecoder(resp.Body)
+	decoder.Decode(&data)
 
-		for _, item := range links {
-			link := item.(map[string]interface{})
-			if link["rel"] == "alternate" && link["type"] == "text/html" {
-				client.Privmsg(buffer, link["href"].(string))
-			}
+	// Pick a random video
+	feed := data["feed"].(map[string]interface{})
+	videos := feed["entry"].([]interface{})
+	video := videos[rand.Intn(len(videos))].(map[string]interface{})
+	links := video["link"].([]interface{})
+
+	for _, item := range links {
+		link := item.(map[string]interface{})
+		if link["rel"] == "alternate" && link["type"] == "text/html" {
+			client.Privmsg(buffer, link["href"].(string))
 		}
 	}
 }
